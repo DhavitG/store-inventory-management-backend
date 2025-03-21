75ca625c4af16c2ea8e4615334ba2cc19d23c112
DhavitG <gdhavit@gmail.com>
2025-03-22 02:49:23 +0530

    Controllers and routes setup

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+const connectDB = require("./db/connect");
+const productsRouter = require("./routes/product");
+
 const notFoundMiddleware = require("./middlewares/not-found");
 const errorHandlerMiddleware = require("./middlewares/error-handler");
 
@@ -12,6 +15,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send(`<h1>Store API</h1><a href="/api/v1/products">products route</a>`);
 });
+
+app.use("/api/v1/products", productsRouter);
 
 // products route
 
@@ -23,6 +28,7 @@ const port = process.env.PORT || 5003;
 const start = async () => {
   try {
     //connectDB
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, console.log(`Server is listening port ${port}...`));
   } catch (e) {
     console.log(e);
---
+++ routes/product.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+const router = Router();
+
+const {
+  getAllProducts,
+  getAllProductsStatic,
+} = require("../controllers/product");
+
+router.route("/").get(getAllProducts);
+
+router.route("/static").get(getAllProductsStatic);
+
+module.exports = router;
