e2f561e394cdaf750d29167c7a268399bd6c41b4
DhavitG <gdhavit@gmail.com>
2025-03-24 05:27:44 +0530

    db filtering methods

--- controllers/product.js
+++ controllers/product.js
@@ -1,12 +1,32 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  throw new Error("successful in throwing errors");
-  res.status(200).json({ msg: "Products testing route" });
+  const search = "ab"; // everything with an ab
+  const products = await Product.find({
+    name: { $regex: search, $options: "i" },
+  });
+  res.status(200).json({ products });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "Products testing route" });
+  const { featured, company, name } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    // queryObject.featured = featured;
+    queryObject.featured = featured === "true" ? true : false; // just to get it in boolean
+  }
+
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products });
 };
 
 module.exports = {
